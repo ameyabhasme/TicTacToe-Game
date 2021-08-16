1491aeb6e3b59bd2217eda8a2a4204eab79bd8a2
ameyabhasme <ameyabhasme5@gmail.com>
2021-08-17 01:23:08 +0530

    Added: Ability for player to choose letter

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,13 +1,18 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
 	static char board[] = new char[10];
+	static char player, computer;
+	static final Scanner SC = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game");
 		init();
 		createBoard();
+		chooseSign();
 	}
 
 	public static void init() {
@@ -24,5 +29,20 @@ public class TicTacToeGame {
 		System.out.println(board[6] + " | " + board[7] + " | " + board[8]);
 		System.out.println("Board is created!");
 	}
-
+	
+	public static void chooseSign() {
+		while (player != 'X' && player != 'O') {
+			System.out.print("Choose a sign between 'X' and 'O': ");
+			char userChoice = SC.next().charAt(0);
+			if (userChoice == 'O' || userChoice == 'o') {
+				player = 'O';
+				computer = 'X';
+			} else if (userChoice == 'X' || userChoice == 'x') {
+				player = 'X';
+				computer = 'O';
+			} else
+				System.out.println("Invalid choice!");
+		}
+		System.out.println("Player sign: " + player + "\n" + "Comouter sign: " + computer);
+	}
 }
