16d86e1e6ab61dfa9b806960ca68cb4f7a4b0cca
ameyabhasme <ameyabhasme5@gmail.com>
2021-08-17 01:12:55 +0530

    Added: Create TicTacToe Board

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -2,7 +2,27 @@ package com.bridgelabz;
 
 public class TicTacToeGame {
 
+	static char board[] = new char[10];
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game");
+		init();
+		createBoard();
 	}
+
+	public static void init() {
+		for (int i = 0; i < 10; i++) {
+			board[i] = ' ';
+		}
+	}
+
+	public static void createBoard() {
+		System.out.println(board[0] + " | " + board[1] + " | " + board[2]);
+		System.out.println("---------");
+		System.out.println(board[3] + " | " + board[4] + " | " + board[5]);
+		System.out.println("---------");
+		System.out.println(board[6] + " | " + board[7] + " | " + board[8]);
+		System.out.println("Board is created!");
+	}
+
 }
